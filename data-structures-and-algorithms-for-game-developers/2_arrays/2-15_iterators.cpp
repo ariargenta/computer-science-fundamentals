44c94344d367e27a3a4d1ed49ee1c3cf15cb3c82
Aria Argenta Silva Castañeda <55609849+ariargenta@users.noreply.github.com>
2025-11-04 17:43:37 -0600

    feat: iterators

--- data-structures-and-algorithms-for-game-developers/2_arrays/2-15_iterators.cpp
+++ data-structures-and-algorithms-for-game-developers/2_arrays/2-15_iterators.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
 
 void printVector(std::vector<int>& array) {
     std::cout
@@ -10,15 +13,13 @@ void printVector(std::vector<int>& array) {
         << (int)array.capacity()
         << ") - ";
 
-    for(int i = 0; i < (int)array.size(); ++i) {
-        std::cout << array[i] << " ";
-    }
+    std::ostream_iterator<int> output(std::cout, " ");
 
     std::cout << std::endl;
 }
 
-int main(int argc, char** argv) {
-    std::cout << "STL vector example" << std::endl;
+int main(int argc, char* argv[]) {
+    std::cout << "STL vector example 2: Iterators" << std::endl;
     std::cout << "Data Structures for Game Developers" << std::endl;
     std::cout << "Aria Argenta" << std::endl << std::endl;
 
@@ -29,34 +30,37 @@ int main(int argc, char** argv) {
     array.push_back(20);
     array.push_back(30);
     array.push_back(40);
+    array.push_back(50);
 
-    std::cout << "Inserted into vector.";
+    std::vector<int> array2;
+
+    for(int i = 0; i < 5; ++i) {
+        array2.push_back(0);
+    }
+
+    std::copy(array.begin(), array.end(), array2.begin());
+
+    std::cout << "Inserted into vector: ";
 
     printVector(array);
 
+    std::cout
+        << "Accumulate: "
+        << std::accumulate(array.begin(), array.end(), 0)
+        << std::endl;
+
     array.pop_back();
     array.pop_back();
 
-    std::cout << "Popped two from vector.";
+    std::cout << "Popped two from vector: ";
 
     printVector(array);
 
     array.clear();
 
-    std::cout << "Cleared vector.";
+    std::cout << "Cleared vector: ";
 
     printVector(array);
 
     std::cout << std::endl;
-
-    if(array.empty() == true) {
-        std::cout << "Vector is empty.";
-    }
-    else {
-        std::cout << "Vector is NOT empty.";
-    }
-
-    std::cout << std::endl << std::endl;
-
-    return 1;
 }
