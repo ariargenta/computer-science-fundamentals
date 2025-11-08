9129f20a53900edbc43621c46020f5c7f9dca7c6
Aria Argenta Silva Castañeda <55609849+ariargenta@users.noreply.github.com>
2025-11-08 14:40:30 -0600

    feat: node class

---
+++ data-structures-and-algorithms-for-game-developers/5_link-lists/5-1_link-list.cpp
@@ -0,0 +1,12 @@
+template<typename T> class LinkIterator;
+template<typename T> class LinkList;
+
+template<typename T>
+class LinkNode {
+    friend class LinkIterator<T>;
+    friend class LinkList<T>;
+
+    private:
+        T m_data;
+        LinkNode* m_next;
+};
