0c8ad6968ee308556d23c57036ee5870db0f7449
Aria Argenta Silva Castañeda <55609849+ariargenta@users.noreply.github.com>
2025-11-08 14:50:40 -0600

    feat: iterator class

--- data-structures-and-algorithms-for-game-developers/5_link-lists/5-1_link-list.cpp
+++ data-structures-and-algorithms-for-game-developers/5_link-lists/5-1_link-list.cpp
@@ -10,3 +10,46 @@ class LinkNode {
         T m_data;
         LinkNode* m_next;
 };
+
+template<typename T>
+class LinkIterator {
+    public:
+        LinkIterator() {
+            m_node = NULL;
+        }
+
+        ~LinkIterator() {}
+
+        void operator = (LinkNode<T>* node) {
+            m_node = node;
+        }
+
+        T& operator*() {
+            assert(m_node != NULL);
+
+            return m_node -> m_data;
+        }
+
+        void operator++() {
+            assert(m_node != NULL);
+
+            m_node = m_node -> m_next;
+        }
+
+        void operator++(int) {
+            assert(m_node != NULL);
+
+            m_node = m_node -> m_next;
+        }
+
+        bool operator != (LinkNode<T>* node) {
+            return(m_node != node);
+        }
+
+        bool operator == (LinkNode<T>* node) {
+            return(m_node == node);
+        }
+
+    private:
+        LinkNode<T>* m_node;
+};
