4021d7c6fd5829c92b5587e210b98ed86c31f3a1
Aria Argenta Silva Castañeda <55609849+ariargenta@users.noreply.github.com>
2025-11-27 12:45:58 -0600

    feat: depth-first search algorithm

---
+++ data-structures-and-algorithms-for-game-developers/11_graphs/11-1_depth-first-search.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <cassert>
+#include <cstring>
+#include <vector>
+#include <stack>
+
+template<typename T>
+class GraphVertex {
+    public:
+        GraphVertex(T node) : m_node(node) {}
+
+        T GetNode() {
+            return m_node;
+        }
+
+    private:
+        T m_node;
+};
+
+template<typename T>
+class Graph {
+    public:
+        Graph(int numVerts) : m_maxVerts(numVerts), m_adjMatrix(NULL) {
+            assert(numVerts > 0);
+
+            m_vertices.reserve(m_maxVerts);
+
+            m_adjMatrix = new char*[m_maxVerts];
+
+            assert(m_adjMatrix != NULL);
+
+            m_vertVisits = new char[m_maxVerts];
+
+            assert(m_vertVisits != NULL);
+
+            memset(m_vertVisits, 0, m_maxVerts);
+
+            for(int i = 0; i < m_maxVerts; ++i) {
+                m_adjMatrix[i] = new char[m_maxVerts];
+                
+                assert(m_adjMatrix[i] != NULL);
+
+                memset(m_adjMatrix[i], 0, m_maxVerts);
+            }
+        }
+
+        ~Graph() {
+            if(m_adjMatrix != NULL) {
+                for(int i = 0; i < m_maxVerts; ++i) {
+                    if(m_adjMatrix[i] != NULL) {
+                        delete[] m_adjMatrix[i];
+
+                        m_adjMatrix[i] = NULL;
+                    }
+                }
+
+                delete[] m_adjMatrix;
+
+                m_adjMatrix = NULL;
+            }
+
+            if(m_vertVisits != NULL) {
+                delete[] m_vertVisits;
+
+                m_vertVisits = NULL;
+            }
+        }
+
+        bool push(T node) {
+            if((int)m_vertices.size() >= m_maxVerts) {
+                return false;
+            }
+
+            m_vertices.push_back(GraphVertex<T>(node));
+
+            return true;
+        }
+
+        void attachEdge(int index1, int index2) {
+            assert(m_adjMatrix != NULL);
+
+            m_adjMatrix[index1][index2] = 1;
+            m_adjMatrix[index2][index1] = 1;
+        }
+
+        void attachDirectedEdge(int index1, int index2) {
+            assert(m_adjMatrix != NULL);
+
+            m_adjMatrix[index1][index2] = 1;
+        }
+
+        int getNextUnvisitedVertex(int index) {
+            assert(m_adjMatrix != NULL);
+            assert(m_vertVisits != NULL);
+
+            for(int i = 0; i < (int)m_vertices.size(); ++i) {
+                if(m_adjMatrix[index][i] == 1 && m_vertVisits[i] == 0) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        bool DepthFirstSearch(int startIndex, int endIndex) {
+            assert(m_adjMatrix != NULL);
+            assert(m_vertVisits != NULL);
+
+            m_vertVisits[startIndex] = 1;
+
+            std::cout << m_vertices[startIndex].GetNode();
+
+            std::stack<int> searchStack;
+
+            int vert = 0;
+
+            searchStack.push(startIndex);
+
+            while(searchStack.empty() != true) {
+                vert = getNextUnvisitedVertex(searchStack.top());
+
+                if(vert == -1) {
+                    searchStack.pop();
+                }
+                else {
+                    m_vertVisits[vert] = 1;
+
+                    std::cout << m_vertices[vert].GetNode();
+
+                    searchStack.push(vert);
+                }
+
+                if(vert == endIndex) {
+                    memset(m_vertVisits, 0, m_maxVerts);
+
+                    return true;
+                }
+            }
+
+            memset(m_vertVisits, 0, m_maxVerts);
+
+            return false;
+        }
+
+    private:
+        std::vector<GraphVertex<T>> m_vertices;
+        int m_maxVerts;
+        char** m_adjMatrix;
+        char* m_vertVisits;
+};
+
+int main(int argc, char** argv) {
+    std::cout << "Graphs - Depth First Search" << std::endl;
+    std::cout << "Chapter 11: Graphs" << std::endl;
+    std::cout << std::endl;
+
+    Graph<char> demoGraph(6);
+
+    demoGraph.push('A');
+    demoGraph.push('B');
+    demoGraph.push('C');
+    demoGraph.push('D');
+    demoGraph.push('E');
+    demoGraph.push('F');
+    demoGraph.attachEdge(0, 2); //A2C & C2A
+    demoGraph.attachEdge(0, 3); //A2D & D2A
+    demoGraph.attachEdge(1, 4); //B2E & E2B
+    demoGraph.attachEdge(2, 5); //C2F & F2C
+
+    std::cout << "Depth-First Search nodes visited: ";
+
+    int result = demoGraph.DepthFirstSearch(0, 3);
+
+    std::cout << std::endl << std::endl;
+
+    if(result == 1) {
+        std::cout << "Path from A to D found";
+    }
+    else {
+        std::cout << "Path from A to D NOT found!";
+    }
+
+    std::cout << std::endl << std::endl;
+
+    return 1;
+}
