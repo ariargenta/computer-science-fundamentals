a1be79662310366b78aa3881e06766a0574c7465
Aria Argenta Silva Castañeda <55609849+ariargenta@users.noreply.github.com>
2025-12-09 19:01:08 -0600

    feat: demo scene graph

--- data-structures-and-algorithms-for-game-developers/13_scene-management/13-1_scene-graph.cpp
+++ data-structures-and-algorithms-for-game-developers/13_scene-management/13-1_scene-graph.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <stdio.h>
 #include <cmath>
 #include <vector>
+
+#include <gl/glut.h>
 
 enum PLANE_STATUS {
     PLANE_FRONT = 0
@@ -609,3 +612,129 @@ class SceneGraph {
     private:
         Node* m_root;
 };
+
+SceneGraph g_sceneGraph;
+
+void Resize(int width, int height) {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    gluPerspective(45, width / height, 0.1, 200.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void KeyDown(unsigned char key, int x, int y) {
+    switch(key) {
+        case 27:
+            exit(0);
+
+            break;
+    }
+}
+
+bool InitializeApp() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+
+    float diffuseLight[] = {0.8f, 0.8f, 0.8f, 1.0f};
+    float specularLight[] = {1.0f, 1.0f, 1.0f, 1.0f};
+    float LightPosition[] = {1.0f, 0.0f, 8.0f, 1.0f};
+
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
+    glLightfv(GL_LIGHT0, GL_POSITION, LightPosition);
+    glEnable(GL_LIGHT0);
+
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, specularLight);
+    glMateriali(GL_FRONT, GL_SININESS, 128);
+
+    TransformationNode* group1 = NULL;
+    TransformationNode* group2 = NULL;
+    TransformationNode* spherePos1 = NULL;
+    TransformationNode* spherePos2 = NULL;
+    TransformationNode* spherePos3 = NULL;
+    TransformationNode* spherePos4 = NULL;
+    TransformationNode* spherePos5 = NULL;
+    TransformationNode* spherePos6 = NULL;
+    SphereNode* sphere1 = NULL;
+    SphereNode* sphere2 = NULL;
+    SphereNode* sphere3 = NULL;
+    SphereNode* sphere4 = NULL;
+    SphereNode* sphere5 = NULL;
+    SphereNode* sphere6 = NULL;
+
+    group1 = new TransformationNode(Vector3D(5, 0, 0));
+    spherePos1 = new TransformationNode(Vector3D(-1, 0, 0));
+    spherePos2 = new TransformationNode(Vector3D(1, 0, 0));
+    spherePos3 = new TransformationNode(Vector3D(0, 0, -1));
+    sphere1 = new SphereNode(1, 20, 20, 1, 0, 0);
+    sphere2 = new SphereNode(1, 20, 20, 0, 1, 0);
+    sphere3 = new SphereNode(1, 20, 20, 0, 0, 1);
+
+    spherePos1 -> AddChild(sphere1);
+    spherePos2 -> AddChild(sphere2);
+    spherePos3 -> AddChild(sphere3);
+    group1 -> AddChild(spherePos1);
+    group1 -> AddChild(spherePos2);
+    group1 -> AddChild(spherePos3);
+
+    group2 = new TransformationNode(Vector3D(-5, 0, 0));
+    spherePos4 = new TransformationNode(Vector3D(-1, 0, 0));
+    spherePos5 = new TransformationNode(Vector3D(1, 0, 0));
+    spherePos6 = new TransformationNode(Vector3D(0, 0, -1));
+    sphere4 = new SphereNode(1, 20, 20, 1, 1, 0);
+    sphere5 = new SphereNode(1, 20, 20, 0, 1, 1);
+    sphere6 = new SphereNode(1, 20, 20, 1, 0, 1);
+
+    spherePos4 -> AddChild(sphere4);
+    spherePos5 -> AddChild(sphere5);
+    spherePos6 -> AddChild(sphere6);
+    group2 -> AddChild(spherePos4);
+    group2 -> AddChild(spherePos5);
+    group2 -> AddChild(spherePos6);
+
+    g_sceneGraph.AddNode(group1);
+    g_sceneGraph.AddNode(group2);
+
+    return true;
+}
+
+void ShutdownApp() {
+    g_sceneGraph.Release();
+}
+
+void RenderScene() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(0, 5, -20, 0, 0, 0, 0, 1, 0);
+
+    g_sceneGraph.Process();
+
+    glutSwapBuffers();
+    glutPostRedisplay();
+}
+
+int main(int argc, char* argv[]) {
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInit(&arg, argc);
+    glutCreateWindow("Scene Graph");
+    glutDisplayFunc(RenderScene);
+    glutReshapeFunc(Resize);
+    glutKeyboardFunc(KeyDown);
+
+    if(InitializeApp() == true) {
+        glutMainLoop();
+    }
+    else {
+        printf("Error in InitializeApp()!\n\n");
+    }
+
+    ShutdownApp();
+
+    return 1;
+}
