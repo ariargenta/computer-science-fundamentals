3dd32f0e9d664fe10ef1db486d00a26e917ec543
ariargenta <55609849+ariargenta@users.noreply.github.com>
2026-01-09 17:42:02 -0600

    feat: data abstraction - lists

--- structure-and-interpretation-of-computer-programs/2_building-abstractions-with-data/2-2_hierarchical-data-closure-property/2-2-1_representing-sequences.lisp
+++ structure-and-interpretation-of-computer-programs/2_building-abstractions-with-data/2-2_hierarchical-data-closure-property/2-2-1_representing-sequences.lisp
@@ -1,3 +1,10 @@
+;;; Conventional interfaces - lists
+; A list is a data object that can hold an arbitrary number of ordered items
+; Lists are closed under operations of cons and cdr
+; - car-part of a pair in sequence - holds an item
+; - cdr-part of a pair in sequence - holds a pointer to rest of list
+; - Empty-list nil - signals no more pairs, end of the list
+;
 ; (list ⟨a_1⟩ ⟨a_2⟩... ⟨a_n⟩)
 ; ==
 ; (cons ⟨a_1⟩
@@ -5,6 +12,26 @@
 ;             (cons ...
 ;                    (cons ⟨a_n⟩
 ;                           nil)...)))
+;
+; (cons <el1> <el2>)
+;    +-----+-----+
+;    |     |     |
+; -->|     |   --|--> <el2>
+;    |  |  |     |
+;    +--|--+-----+
+;       |
+;       v
+;     <el1>
+;
+; (list <el1> <el2> ... <eln>)
+;    +---+---+   +---+---+           +---+---+
+; -->| o | o-+-->| o | o-+--> ... -->| o | / |
+;    +---+---+   +---+---+           +---+---+
+;      ↓           ↓                   ↓
+;    <el1>       <el2>               <eln>
+;
+; (null? <z>)
+;     ==> #t if <z> evaluates to empty list
 (defparameter one-through-four (list 1 2 3 4))
 
 (princ one-through-four) (terpri)
@@ -17,6 +44,65 @@
 (princ (cons 5 one-through-four)) (terpri)
 
 ;; List operations
+; cons'ing up a list
+(defun enumerate-interval (from to)
+    (if (> from to)
+        nil
+        (adjoin from (enumerate-interval (+ 1 from) to))))
+
+; cdr'ing down a list
+(defun list-reference (lst n)
+    (if (= n 0)
+        (first lst)
+        (list-ref (rest lst)
+                  (- n 1))))
+
+(defun length-list (lst)
+    (if (null lst)
+        0
+        (+ 1 (length-list (rest lst)))))
+
+(defun copy (lst)
+    (if (null lst)
+        nil                             ; base case
+        (adjoin (first lst)             ; recursion
+                (copy (rest lst)))))
+
+(defun append-list (list1 list2)
+    (cond ((null list1) list2)                  ; base
+          (t
+              (adjoin (first list1)             ; recursion
+                      (append-list (rest list1)
+                                   list2)))))
+
+(defparameter group (list p1 p2 p3 p4 p5 p6 p7 p8 p9))
+
+(defun stretch-group (gp sc)
+    (if (null gp)
+        nil
+        (adjoin (stretch-point (first gp) sc)
+                (stretch-group (rest gp) sc))))
+
+; Transforming a list
+(defun add-x (gp)
+    (if (null gp)
+        0
+        (+ (point-x (first gp))
+           (add-x (rest gp)))))
+
+(defun add-y (gp)
+    (if (null gp)
+        0
+        (+ (point-y (first gp))
+           (add-y (rest gp)))))
+
+(defun centroid (gp)
+    (let ((x-sum (add-x gp))
+          (y-sum (add-y gp))
+          (how-many (length-list gp)))
+      (make-point (/ x-sum how-many)
+                  (/ y-sum how-many))))
+
 (defun list-ref (items n)
     (if (= n 0)
         (car items)
@@ -45,10 +131,10 @@
 (princ (append squares odds)) (terpri)
 (princ (append odds squares)) (terpri)
 
-(defun append-list (list1 list2)
+(defun appendList (list1 list2)
     (if (null list1)
         list2
-        (cons (car list1) (append-list (cdr list1) list2))))
+        (cons (car list1) (appendList (cdr list1) list2))))
 
 ;; Mapping over lists
 (defun scale-list (items factor)
@@ -72,3 +158,7 @@
 (defun scaleList (items factor)
     (mapcar (lambda (x) (* x factor))
             items))
+
+; - There are conventional ways of grouping elements together into compound data structures
+; - The procedures that manipulate these data structures tend to have a form that mimics the actual data structure
+; - Compound data structures rely on an inductive format in much the same way recursive procedures do. We can often deduce properties of compound data structures in analogy to our analysis of recursive procedures by using induction
